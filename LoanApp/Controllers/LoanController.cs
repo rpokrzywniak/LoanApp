aeca76bf2cbb20d604c34793ad8f5b07e22c7ffd
Robert Pokrzywniak <robert.pokrzywniak@gmail.com>
2020-12-19 10:24:41 +0100

    Add project files.

---
+++ LoanApp/Controllers/LoanController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace LoanApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoanController : ControllerBase
+    {
+        private readonly ILogger<LoanController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public LoanController(ILogger<LoanController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<double> GetLoanTypes()
+        {
+            _configuration.GetSection("Loan");
+            return 2;
+        }
+    }
+}
